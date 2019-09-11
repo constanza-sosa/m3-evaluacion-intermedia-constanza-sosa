4356383bdacab453158bbd4751045f90944cccbe
Constanza Sosa <constanza.sosa.g@gmail.com>
2019-09-11 10:04:41 +0200

    Add docs

---
+++ docs/precache-manifest.c6e86c9917f4c31e53a18363509fd04d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3f64364106a644d488d6b8b72b0247",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4323689b7a50c73d4c64",
+    "url": "./static/css/main.f5268db9.chunk.css"
+  },
+  {
+    "revision": "602f08c074fd0c50776e",
+    "url": "./static/js/2.a5ddb538.chunk.js"
+  },
+  {
+    "revision": "4323689b7a50c73d4c64",
+    "url": "./static/js/main.dff1db50.chunk.js"
+  },
+  {
+    "revision": "a9a153e75342472896db",
+    "url": "./static/js/runtime~main.14c946dd.js"
+  }
+]);
